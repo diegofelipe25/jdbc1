315e73573600d5d454efc546de52182eb315f447
diegofelipe25 <diego_felipe25@outlook.com>
2022-11-22 16:30:24 -0300

    vbbb

--- src/db/Program.java
+++ src/db/Program.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 public class Program {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		Connection conn = DB.getConnection();
 		DB.closeConnection();
 	}
